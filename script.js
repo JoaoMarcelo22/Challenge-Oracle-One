b6dec52fccd9c58fa0d3468817e6868e79dbb2ab
João Marcelo <joao_marcelo.22@hotmail.com>
2022-01-31 23:26:12 -0300

    mudanças JS

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
+//const btn = document.querySelector("#cripto");
+//btn.addEventListener("click", function(e){
+    //e.preventDefault();
 
-/* Regras Codificador: 
-"e" é convertido para "enter" 
-"i" é convertido para "imes"
-"a" é convertido para "ai"
-"o" é convertido para "ober"
-"u" é convertido para "ufat"
-Apenas letras minúsculas
-Não permite acentuação   
-*/
+    //const name = document.querySelector("#texto");
 
-/* Regras Decodificador: 
-"enter" é convertido para "e" 
-"imes" é convertido para "i"
-"ai" é convertido para "a"
-"ober" é convertido para "o"
-"ufat" é convertido para "u"
-Apenas letras minúsculas
-Não permite acentuação     
-*/
+  //  const valor = name.value;
+
+//});
+
+const texto = document.querySelector("#texto").value;

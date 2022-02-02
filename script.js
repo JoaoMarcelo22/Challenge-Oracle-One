83481a70c11cf3821dabd9a8374c1d2dde6ee6b7
João Marcelo <joao_marcelo.22@hotmail.com>
2022-02-01 23:43:46 -0300

    script

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//const btn = document.querySelector("#cripto");
-//btn.addEventListener("click", function(e){
-    //e.preventDefault();
+const texto = document.querySelector('#texto').value;
 
-    //const name = document.querySelector("#texto");
 
-  //  const valor = name.value;
+const textoModificado = texto.replace(/[a\á\à\â\ã]/gi, '01').replace(/[e\é\è\ê]/gi, '05').replace(/[i\í\ì\î]/gi, '09').replace(/[o\ó\ò\ô\õ]/gi, '15').replace(/[u\ú\ù\û]/gi, '21');
 
-//});
+let textoResultado = document.querySelector('#msg');
 
-const texto = document.querySelector("#texto").value;
+textoResultado.value = textoModificado;
+
+
+console.log(textoModificado);

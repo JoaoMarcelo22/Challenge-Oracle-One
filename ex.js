2a3f848339a9260e7f96ad86022f5ac043c30fb2
João Marcelo <joao_marcelo.22@hotmail.com>
2022-02-02 23:05:37 -0300

    mm

---
+++ ex.js
@@ -0,0 +1,7 @@
+//var codigo = document.getElementById('#mensagem');
+var codigo = "arroz";
+codigo.value;
+
+console.log(codigo);
+
+var final = document.querySelector('#endregionrecebe')
--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const texto = document.querySelector('#texto').value;
+//const texto = document.getElementById('mensagem').value;
+const texto = "eu quero ir para casa";
+console.log(texto);
 
 
 const textoModificado = texto.replace(/[a\á\à\â\ã]/gi, '01').replace(/[e\é\è\ê]/gi, '05').replace(/[i\í\ì\î]/gi, '09').replace(/[o\ó\ò\ô\õ]/gi, '15').replace(/[u\ú\ù\û]/gi, '21');
 
-let textoResultado = document.querySelector('#msg');
+//let textoResultado = document.querySelector('#recebe');
 
-textoResultado.value = textoModificado;
+var textoResultado = textoModificado;
 
 
 console.log(textoModificado);
+
+var palavraChave = ['01', '05', '09', '15', '21'];
+
+var verificar = palavraChave.some(elementoArray => textoResultado.includes(elementoArray));
+
+
+
+//if (verificar){
+ //   descripitografar();
+//}
+//else {
+ //   alert('a mensagem não esta criptografada');
+//}
